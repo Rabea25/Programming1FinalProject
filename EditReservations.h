2a8915806dcba7d6bb8985125fa72c310d6d1ac7
Heba2627 <alskaradhm9@gmail.com>
2024-12-25 22:45:50 +0200

    Update EditReservations.h

--- EditReservations.h
+++ EditReservations.h
@@ -3,123 +3,159 @@
 #include <sstream>
 #include <ctime>
 #include <vector>
+#include <string>
+#include "Validations.h""
+#include "structs.h"
+
 using namespace std;
-
-void EditReservation(string  reservationID)
+void EditReservation()
 {
+
+    cout << "Editing Reservation:" << endl;
+    cout << "Please enter you reservation ID" << endl;
+    string reservationID;
+    cin >> reservationID;
+
     string roomNumber;
-
     //part el reservation.txt
-    ifstream file("Reservation.txt");
-    vector<string> data;
-    string line;
-    while (getline(file, line))
-    {
-        data.push_back(line);
-    }
-    file.close();
+    
 
-    int flag1 = 0;
-    for (int i = 0; i < data.size(); i++)
+    int flag = 0;
+    for (int i = 0; i < reservations.size(); i++)
     {
         //part reservation
-        stringstream x(data[i]);
-        string token;
-        vector<string> PPLDetails;
-
-        while (getline(x, token, ','))
+        if (reservations[i].id == reservationID)
         {
-            PPLDetails.push_back(token);
-        }
-        if (PPLDetails[0] == reservationID)
-        {
-            flag1 = 1;
-            // 3shan part el rooms:
-            roomNumber = PPLDetails[1];
+            flag = 1;
+            roomNumber = reservations[i].room_no;
 
             cout << "PLease help us to collect all your data with modifications: \n";
-            cout << "Please enter your name: \n";
-            getline(cin, PPLDetails[3]);
+            cout << "Notice you have only 3 trails to edit anything\n";
 
-            cout << "Please enter your mobile number: \n";
-            getline(cin, PPLDetails[8]);
-
-            cout << "Please enter your email address: \n";
-            getline(cin, PPLDetails[7]);
-
-            //Reservations Date::
-
-
-
-
-            cout << "Please enter your number of nights: \n";
-            getline(cin, PPLDetails[5]);
-
-            //Updating el data 
-            data[i] = "";
-            for (int j = 0; j < PPLDetails.size(); j++)
+            for (int h = 0; h < 3; h++)
             {
-                data[i] += PPLDetails[j];
-                //el commas
-                if (j < PPLDetails.size() - 1)
-                    data[i] += ",";
+                cout << "Please enter your name: \n";
+                string name;
+                getline(cin, name);
+                if (Validations::NameValidation(name))
+                {
+                    reservations[i].name = name;
+                    break;
+                }
+                else
+                {
+                    cout << "Invalid name and you have " << 3 - h << " trails left\n";
+
+                }
+                if (h == 2)
+                {
+                    "You have consumed all your trails please try again later\n";
+                    return;
+                }
+            }
+
+            for (int h = 0; h < 3; h++)
+            {
+                cout << "Please enter your phone number: \n";
+                string phoneNumber;
+                getline(cin, phoneNumber);
+                if (Validations::PhoneNumberValidaion(phoneNumber))
+                {
+                    reservations[i].phone = phoneNumber;
+                    break;
+                }
+                else
+                {
+                    cout << "Invalid phone Number and you have " << 3 - h << " trails left\n";
+
+                }
+                if (h == 2)
+                {
+                    "You have consumed all your trails please try again later\n";
+                    return;
+                }
+            }
+
+            for (int h = 0; h < 3; h++)
+            {
+                cout << "Please enter your Email Address: \n";
+                string EmailAddress;
+                getline(cin, EmailAddress);
+                if (Validations::EmailValidation(EmailAddress))
+                {
+                    reservations[i].email = EmailAddress;
+                    break;
+                }
+                else
+                {
+                    cout << "Invalid Email Address and you have " << 3 - h << " trails left\n";
+
+                }
+                if (h == 2)
+                {
+                    "You have consumed all your trails please try again later\n";
+                    return;
+                }
+            }
+
+            for (int h = 0; h < 3; h++)
+            {
+                cout << "Please enter your Number of Nights: \n";
+                string NoOFNights;
+                getline(cin, NoOFNights);
+                if (Validations::NightsValidation(NoOFNights))
+                {
+                    reservations[i].nights = NoOFNights;
+                    break;
+                }
+                else
+                {
+                    cout << "Invalid Number of nights and you have " << 3 - h << " trails left\n";
+
+                }
+                if (h == 2)
+                {
+                    "You have consumed all your trails please try again later\n";
+                    return;
+                }
             }
             break;
 
-        }
+        }   
     }
-    //Update el file zat nafso
-    if (flag1)
-    {
-        ofstream reservationFile("Reservation.txt");
-        for (const string& res : data)
-        {
-            reservationFile << res << endl;
-        }
-        reservationFile.close();
-        cout << "Reservation updated successfully.\n";
-    }
-    else
+   
+    if (!flag)
     {
         cout << "Reservation ID not found.\n";
+        return;
     }
-
-
-    //part el room.txt
-    ifstream file2("Room.txt");
-    vector<string> data2;
-    string line2;
-    while (getline(file2, line2))
+   
+    for (int i = 0; i < rooms.size(); i++)
     {
-        data2.push_back(line2);
-    }
-    file2.close();
-
-    int flag2 = 0;
-    for (int i = 0; i < data2.size(); i++)
-    {
-        //part el room
-        stringstream y(data2[i]);
-        string token2;
-        vector<string> Rooms;
-
-        while (getline(y, token2, ' '))
+        
+        if (rooms[i].room_no == roomNumber)
         {
-            Rooms.push_back(token2);
-        }
-        if (Rooms[0] == roomNumber)
-        {
-            flag2 = 1;
-            cout << "Please enter the new room category: \n";
-            getline(cin, Rooms[2]);
-
-            data2[i] = "";
-            for (int j = 0; j < Rooms.size(); j++)
+           
+            for (int h = 0; h < 3; h++)
             {
-                data2[i] += Rooms[j];
-                //el spaces
-                if (j < Rooms.size() - 1)
-                    data2[i] += " ";
+                cout << "Please enter your Room category (S:SeaView, G:GardenView, L:LakeView): \n";
+                string roomCategory;
+                getline(cin, roomCategory);
+                if ( roomCategory == "S" || roomCategory == "G" || roomCategory == "L" )
+                {
+                    rooms[i].type = roomCategory;
+                    break;
+                }
+                else
+                {
+                    cout << "Invalid room category and you have " << 3 - h << " trails left\n";
+
+                }
+                if (h == 2)
+                {
+                    "You have consumed all your trails please try again later\n";
+                    return;
+                }
             }
             break;
         }
@@ -127,20 +163,7 @@ void EditReservation(string  reservationID)
 
     }
 
-    if (flag2)
-    {
-        ofstream roomsFile("Rooms.txt");
-        for (const string& room : data2)
-        {
-            roomsFile << room << endl;
-        }
-        roomsFile.close();
-        cout << "Room category updated successfully.\n";
-    }
-    else
-    {
-        cout << "Room number not found.\n";
-    }
 
-
+    cout << "Details updated successfully\n";
+    sortReservations();
 }
